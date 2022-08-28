3b5280be75848d6bc767186f425df1da765f9d75
mayki-douglas <mayki.douglas1@gmail.com>
2022-08-28 18:16:43 -0300

    script de Checkbox de senha

---
+++ src/View/js/login.js
@@ -0,0 +1,8 @@
+function showPassword(){
+    var password = document.getElementById("password");
+    if(password.type == "password"){
+        password.type="text";
+    } else {
+        password.type="password";
+    }
+}
